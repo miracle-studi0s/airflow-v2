665739dcd95441ae10c6340009f9a8460e19dbbf
shialexx <artemiliushchenko93@gmail.com>
2024-09-13 15:07:59 +0300

    fixed some namings and reworked includes in base entity

--- airflow-v2/sdk/entities/base_entity.h
+++ airflow-v2/sdk/entities/base_entity.h
@@ -3,12 +3,10 @@
 #include <string>
 
 #include "../game_data.h"
+#include "../math/vector3d.h"
 
 #include "handle.h"
-
 #include "entity_instance.h"
-
-#include "../math/vector3d.h"
 
 enum base_entity_instance_vfuncs : uintptr_t
 {
@@ -17,13 +15,13 @@ enum base_entity_instance_vfuncs : uintptr_t
 
 class c_base_entity : public c_entity_instance
 {
-	SCHEMA(int, m_team_num, fnv_hash("C_BaseEntity->m_iTeamNum"));
-	SCHEMA(c_handle, m_owner_entity, fnv_hash("C_BaseEntity->m_hOwnerEntity"));
-	SCHEMA(int, m_flags, fnv_hash("C_BaseEntity->m_fFlags"));
-	SCHEMA(int, m_move_type, fnv_hash("C_BaseEntity->m_MoveType"));
-	SCHEMA(int, m_actual_move_type, fnv_hash("C_BaseEntity->m_nActualMoveType"));
-	SCHEMA(int, m_health, fnv_hash("C_BaseEntity->m_iHealth"));
-	SCHEMA(vector3d, m_velocity, fnv_hash("C_BaseEntity->m_vecVelocity"));
+	SCHEMA(int, team_num, fnv_hash("C_BaseEntity->m_iTeamNum"));
+	SCHEMA(c_handle, owner_entity, fnv_hash("C_BaseEntity->m_hOwnerEntity"));
+	SCHEMA(int, flags, fnv_hash("C_BaseEntity->m_fFlags"));
+	SCHEMA(int, move_type, fnv_hash("C_BaseEntity->m_MoveType"));
+	SCHEMA(int, actual_move_type, fnv_hash("C_BaseEntity->m_nActualMoveType"));
+	SCHEMA(int, health, fnv_hash("C_BaseEntity->m_iHealth"));
+	SCHEMA(vector3d, velocity, fnv_hash("C_BaseEntity->m_vecVelocity"));
 
 	bool is_weapon();
 };
